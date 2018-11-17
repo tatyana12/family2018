47f79fedba07188b588a02d25a3418e9fda0b796
tatyana12 <tatyana_pol@yahoo.com>
2018-11-17 13:47:03 -0800

    M

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import data from './events.json';
 
