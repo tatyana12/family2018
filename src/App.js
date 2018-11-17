b762e1b03ba7d6ffbae2c1230194cde7be388590
tatyana12 <tatyana_pol@yahoo.com>
2018-11-17 15:53:34 -0800

    M

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             { data.map(function(people){
                 return <li>{people.id} - {people.name}</li>
              })}
-              }
+               }}}
 
 
 
