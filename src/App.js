e2c0ddabc3420370a04714327ba4812d87879b6d
tatyana12 <tatyana_pol@yahoo.com>
2018-11-17 23:17:12 -0800

    M

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import data from './events.json';
 import One from "./One";
 import Two from "./Two";
+import Index from "./Index";
+
 //const Index = () => <h2>Home</h2>;
 // const One = () => <h2>One</h2>;
 //const Two = () => <h2>Two</h2>;
