1d7128184605508c14d2b080268a07b8fd2e349e
tatyana12 <tatyana_pol@yahoo.com>
2018-12-29 22:56:37 -0800

    M

--- src/Three.js
+++ src/Three.js
@@ -14,11 +14,12 @@ class Three  extends Component {
                                                         return (
                                                                 <ul>
                   <li>Name: {people.name}</li>
-                  <li>Presenter: {people.presenter}</li>
+                  <li>Relationship: {people.presenter}</li>
                   <li>Room: {people.room}</li>
-                  <li>Sumary: {people.summary}</li>
-                  <li>Date: {people.date}</li>
-                  <li>Time: {people.time}</li>
+                  <li>Favorite color: {people.color}</li>
+                  <li>Birthday info: {people.summary}</li>
+                     Date: {people.date}
+                     Time: {people.time}
                 </ul>
                                                         )
                                                 }
